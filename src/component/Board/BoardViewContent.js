eae43e9f549b6a5f76482e41d7f5b31fe8d6211c
suahcho <sacho0929@gmail.com>
2023-11-10 14:33:04 +0900

    feat comment

--- src/component/Board/BoardViewContent.js
+++ src/component/Board/BoardViewContent.js
@@ -2,6 +2,7 @@ import './BoardDetail.css'
 import React, {useState, useEffect} from "react";
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
+import Comment from '../Comment/Comment';
 
 const BoardViewContent = ({boardId}) =>{
     // 게시판 상세정보 데이터
@@ -107,6 +108,8 @@ const BoardViewContent = ({boardId}) =>{
                 {!edit && (<textarea defaultValue={boardData[0].content} onChange={handlerChangeContent}></textarea>)}
                 
             </div>
+            <Comment boardId={boardId}/>
+            
             {/* 버튼 목록 수정 삭제 수정완료 */}
             <div className="BoardViewButtons">
                 <Link to={'/board'} ><input type="button" id="back" className="list" value="목록"/></Link>
